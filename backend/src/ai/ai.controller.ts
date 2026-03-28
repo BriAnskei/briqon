5e8d15b63a41d6c77f52aa214fc5d25c36ede9d6
Brian Gierze Pogie <brianebrahim7@gmail.com>
2026-03-28 21:21:30 +0800

    created schedule parser module for schedule(mode 1) response

--- backend/src/ai/ai.controller.ts
+++ backend/src/ai/ai.controller.ts
@@ -1,27 +1,31 @@
-import { Body, Controller, Get, Query, Res } from '@nestjs/common';
+// ai.controller.ts
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { AiService } from './ai.service';
 import type { Response } from 'express';
 import { PromptDto } from './dto/prompt.dto';
 
 @Controller('ai')
 export class AiController {
-  constructor(private aiServicse: AiService) {}
+  constructor(private aiService: AiService) {}
 
-  @Get()
+  @Post()
   async getResponse(@Body() body: PromptDto, @Res() res: Response) {
     const { prompt } = body;
 
     res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
     res.setHeader('Transfer-Encoding', 'chunked');
 
-    let response = '';
-
-    await this.aiServicse.generateSchdule(prompt, (chunk) => {
+    let response: string = '';
+    await this.aiService.generateSchdule(prompt, (chunk) => {
       response += chunk;
+
+      console.log('response chunk: ', chunk);
       res.write(`data: ${chunk}\n\n`);
     });
 
-    console.log('response: ', response);
+    console.log('full response: ', response);
 
     res.end();
   }
--- backend/src/ai/ai.service.ts
+++ backend/src/ai/ai.service.ts
@@ -5,8 +5,9 @@ import ollama from 'ollama';
 export class AiService {
   async generateSchdule(prompt: string, onChunk: (chunk: string) => void) {
     try {
+      console.log('request recieved: ', prompt);
       const stream = await ollama.chat({
-        model: 'smart-alarm:latest',
+        model: 'briqon',
         messages: [{ role: 'user', content: prompt }],
         stream: true,
       });
